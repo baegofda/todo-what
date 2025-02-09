47a3a2795f9477e1015e8073e42862edac2ecb44
baegofda <molly.daleo3o@gmail.com>
2025-02-10 00:28:43 +0900

    add tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { CookiesProvider } from 'react-cookie';
 import { Toaster } from 'react-hot-toast';
 
 import App from '@/App.tsx';
+import '@/app/styles/index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ tailwind.config.ts
@@ -0,0 +1,6 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+};
+export default config;
